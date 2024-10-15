cb10128bd8c1a763896797dcc846a53f82d8c760
Tina <143371814+Trinketina@users.noreply.github.com>
2024-10-15 00:21:37 -0500

    tweaked some movement
    can no longer adjust velocity midair
    (depending on what we decide, that can be changed later)
    controls are more drifty now
    if we want a more mario kart feel, we can adjust some values mid-game if they press a drift button, not necessary right now

--- Assets/Handlers/CarControl.cs
+++ Assets/Handlers/CarControl.cs
@@ -52,24 +52,33 @@ public class CarControl : MonoBehaviour
         RaycastHit hit;
 
 
-        if (Physics.Raycast(vehicle.position, Vector3.down, out hit, 3))
+        if (Physics.Raycast(vehicle.position, Vector3.down, out hit, transform.localScale.y / 2 +.1f))
         {
+            grounded = true;
             newRot = (Quaternion.FromToRotation(vehicle.up, hit.normal) * vehicle.rotation).eulerAngles;
+        }
+        else
+        {
+            grounded = false;
         }
 
         //horizontal movement
-        if (moveInput != 0)
-            newRot.y += turnInput * turnSpeed;
+        newRot.y += turnInput * turnSpeed;
+        //if (moveInput != 0)
+            
 
-        vehicle.rotation = Quaternion.Lerp(vehicle.rotation, Quaternion.Euler(newRot), lerpSpeed);
+        vehicle.rotation = Quaternion.Lerp(vehicle.rotation, Quaternion.Euler(newRot), lerpSpeed*.5f);
     }
     private void UpdateMovement()
     {
-        Vector3 newVel = vehicle.forward * speed * moveInput;
+        /*Vector3 newVel = vehicle.forward * speed * moveInput;
         newVel.y = rb.velocity.y;
-        rb.velocity = newVel;
+        rb.velocity = newVel;*/
 
-        vehicle.position = Vector3.Lerp(vehicle.position, transform.position, lerpSpeed*.5f);
+        if (grounded)
+            rb.AddForce(vehicle.forward * speed * moveInput, ForceMode.Force);
+
+        vehicle.position = Vector3.Lerp(vehicle.position, transform.position, lerpSpeed);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
