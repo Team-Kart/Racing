43d285e091782b8d546d2e12d37443bad77637b7
Omenys <105769289+Omenys@users.noreply.github.com>
2024-10-23 11:46:13 -0500

    Item Box Triggers
    Basic item box created

---
+++ Assets/Items/Item.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Item acquired!");
+
+            Destroy(gameObject);
+        }
+    }
+}
